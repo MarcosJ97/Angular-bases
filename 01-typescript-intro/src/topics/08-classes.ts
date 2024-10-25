ceee3efd99b99d794ad626e626abd7309aa84114
MarcosJ <marcos.jimenez@conmuta.es>
2024-10-25 13:01:02 +0200

    Primer commit

---
+++ 01-typescript-intro/src/topics/08-classes.ts
@@ -0,0 +1,33 @@
+export class Person {
+  constructor(
+    public firstName: string,
+    public lastName: string,
+    private address: string = 'No Address'
+  ) { }
+}
+
+// export class Hero extends Person {
+//   constructor(
+//     public alterEgo: string,
+//     public age: number,
+//     public realName: string,
+//   ) {
+//     super(realName, 'New York')
+//   }
+// }
+
+export class Hero {
+  constructor(
+    public alterEgo: string,
+    public age: number,
+    public realName: string,
+    public person: Person
+  ) {
+    // this.person = new Person (realName);
+  }
+}
+
+const tony = new Person('Tnoy', 'Stark', 'New York');
+const ironman = new Hero('Ironman', 45, 'Tony', tony);
+
+console.log(ironman);
