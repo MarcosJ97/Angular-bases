fde23581fcc6385ad731426622e66ca246078515
MarcosJ <marcos.jimenez@conmuta.es>
2024-10-29 17:00:22 +0100

    Continuacion seccion

--- 06-heroesApp/src/app/app.module.ts
+++ 06-heroesApp/src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
-import provideAnimationsAsync from '@angular/platform-browser/animations/async';
 
 @NgModule({
   declarations: [
@@ -12,13 +12,12 @@ import provideAnimationsAsync from '@angular/platform-browser/animations/async';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+
     AppRoutingModule,
-    SharedModule
+    SharedModule,
   ],
-  providers: [
-    provideAnimationsAsync()
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
-
 export class AppModule { }
