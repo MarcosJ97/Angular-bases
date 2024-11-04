b5fd886cef10c5ed207d28a861d031eb708019e3
MarcosJ <marcos.jimenez@conmuta.es>
2024-11-04 15:41:16 +0100

    a

--- 07-formsApp/src/app/auth/pages/register-page/register-page.component.ts
+++ 07-formsApp/src/app/auth/pages/register-page/register-page.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { cantBeStrider } from '../../../shared/validators/validators';
 
 @Component({
   templateUrl: './register-page.component.html',
-  styles: [
-  ]
 })
-export class RegisterPageComponent {
 
+export class RegisterPageComponent {
+  public myForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.myForm = this.fb.group({
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required , Validators.email]],
+      username: ['', [Validators.required, cantBeStrider]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      password2: ['', [Validators.required]]
+    });
+  }
+
+  isValidField(field: string) {
+    // TODO: obtener validacion desde un servicio
+  }
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+  }
 }
